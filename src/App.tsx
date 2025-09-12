18886ef10f2d95d6c158e71ad5002b0484d0c414
lakitha <lakitharanwala@gmail.com>
2025-09-13 01:16:16 +0530

    agenda added

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/goagenda" element={<GoAgenda />} /> {/* NEW */}
+            <Route path="/go" element={<Index />} /> {/* NEW */}
+          
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
